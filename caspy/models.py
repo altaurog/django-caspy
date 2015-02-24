7b3d1eff48fe501cb5624f2137a2b3a9b1d898bf
Aryeh Leib Taurog <git@aryehleib.com>
2015-02-24 22:51:07 +0200

    Add Book model

--- caspy/models.py
+++ caspy/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Currency(models.Model):
     code = models.CharField(max_length=8, primary_key=True, db_column='currency_code')
@@ -8,3 +9,13 @@ class Currency(models.Model):
 
     def __str__(self):
         return self.code
+
+class Book(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if self.created_at is None:
+            self.created_at = timezone.now()
+        super(Book, self).save(*args, **kwargs)
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,7 +1,11 @@
+from datetime import timedelta
+import mock
 import pytest
+from django.utils import timezone
 from caspy import models
 
-@pytest.mark.django_db()
+pytestmark = pytest.mark.django_db()
+
 class TestCurrency:
     def test_create_currency(self):
         currency_data = {
@@ -14,5 +18,18 @@ class TestCurrency:
         assert models.Currency.objects.filter(**currency_data).exists()
         assert str(cur_obj) == 'MM'
 
-
-
+class TestBook:
+    def test_create_book(self):
+        now = timezone.now()    # get an aware timezone
+        name = 'Test Book'
+        with mock.patch.object(timezone, 'now', return_value=now):
+            book_obj = models.Book.objects.create(name=name)
+        book_id = book_obj.book_id
+        assert isinstance(book_id, int)
+        assert book_obj.created_at == now
+        data = {'book_id': book_id, 'name': name, 'created_at': now}
+        assert models.Book.objects.filter(**data).exists()
+        with mock.patch.object(timezone, 'now', return_value=now + timedelta(1)):
+            book_obj.save()
+        assert book_obj.created_at == now
+        assert models.Book.objects.filter(**data).exists()
