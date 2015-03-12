c49518b3c59313e530c9592bba31a9d289573b3e
Aryeh Leib Taurog <git@aryehleib.com>
2015-03-13 00:25:12 +0200

    Don't use mock

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,4 +1,3 @@
-import mock
 from datetime import timedelta
 from django.utils import timezone
 from caspy import models
@@ -18,19 +17,16 @@ class TestCurrency:
 
 
 class TestBook:
-    def setup(self):
-        self.now = timezone.now()    # get an aware timezone
-
     def test_null_created_at(self):
         "If created_at isn't set, it should be set to now"
+        before = timezone.now()
         book_obj = models.Book(name='Test Book')
-        with mock.patch.object(timezone, 'now', return_value=self.now):
-            book_obj.set_created_at()
-        assert book_obj.created_at == self.now
+        book_obj.set_created_at()
+        after = timezone.now()
+        assert before <= book_obj.created_at <= after
 
     def test_nonnull_created_at(self):
-        return_value = self.now + timedelta(1)
-        book_obj = models.Book(name='Test Book', created_at=self.now)
-        with mock.patch.object(timezone, 'now', return_value=return_value):
-            book_obj.set_created_at()
-        assert book_obj.created_at == self.now
+        before = timezone.now() - timedelta(1)
+        book_obj = models.Book(name='Test Book', created_at=before)
+        book_obj.set_created_at()
+        assert book_obj.created_at == before
