651746a35fe10b96a3ce70caa8bb97522a5c9a9c
Aryeh Leib Taurog <git@aryehleib.com>
2015-03-14 23:11:05 +0200

    Use explicit dependency injection

--- caspy/static/js/controllers.js
+++ caspy/static/js/controllers.js
@@ -1,9 +1,12 @@
 var caspyApp = angular.module('caspyApp', []);
 
-caspyApp.controller('CurrencyController', function($scope, $http) {
-    $http.get(apiRootUrl).success(function(data) {
-        $http.get(data.currency).success(function(data) {
-            $scope.currencies = data;
+caspyApp.controller('CurrencyController',
+    ['$scope', '$http',
+    function($scope, $http) {
+        $http.get(apiRootUrl).success(function(data) {
+            $http.get(data.currency).success(function(data) {
+                $scope.currencies = data;
+            });
         });
-    });
-});
+    }
+]);
