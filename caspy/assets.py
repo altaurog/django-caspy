af3ca29c725592805f23fcef983537b17e46f36d
Aryeh Leib Taurog <git@aryehleib.com>
2015-03-16 18:10:44 +0200

    Use wildcard

--- caspy/assets.py
+++ caspy/assets.py
@@ -1,6 +1,5 @@
 from django_assets import Bundle, register
 js = Bundle(
-    'caspy/static/js/controllers.js',
-    'caspy/static/js/services.js',
+    'caspy/static/js/*.js',
 )
 register('js_all', js)
