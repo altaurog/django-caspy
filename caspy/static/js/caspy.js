d05cfacb643696e6b28dbf2f01d1d205d5ae4799
Aryeh Leib Taurog <git@aryehleib.com>
2015-03-18 21:27:05 +0200

    Use route/partial

--- caspy/static/js/caspy.js
+++ caspy/static/js/caspy.js
@@ -1,3 +1,17 @@
 (function(){
-angular.module('caspy', ['caspy.currency']);
+var mod = angular.module('caspy',
+    ['ngRoute', 'caspy.server', 'caspy.currency']);
+
+mod.config(['$routeProvider', 'Constants',
+    function($routeProvider, Constants){
+        var proot = Constants.partialsRoot;
+        $routeProvider
+            .when('/currency/', {
+                templateUrl: proot + 'currency-list.html',
+                controller: 'CurrencyController'
+            })
+            .otherwise({
+                redirectTo: '/currency/'
+            });
+    }]);
 })();
