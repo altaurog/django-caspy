c12b28843ee49c6fb6e1f3a4d93973ba9c321cd4
Aryeh Leib Taurog <git@aryehleib.com>
2015-03-19 10:08:31 +0200

    Remove unused bits

--- caspy/static/js/api.js
+++ caspy/static/js/api.js
@@ -12,28 +12,26 @@ mod.factory('caspyAPI',
     function($q, $http, $resource, Constants) {
         var api = {
             root: null
+            , resources: {}
 
-            , resolve: function(name, item) {
+            , resolve: function(name) {
                 var d = $q.defer();
                 if (typeof api.root[name] === 'undefined') {
                     d.reject(new Error(name + ' endpoint not available'));
                 }
                 else {
-                    if (item)
-                        d.resolve(api.root[name] + item + '/');
-                    else
-                        d.resolve(api.root[name]);
+                    d.resolve(api.root[name]);
                 }
                 return d.promise;
             }
 
-            , get_endpoint: function(name, item) {
+            , get_endpoint: function(name) {
                 if (api.root)
-                    return api.resolve(name, item);
+                    return api.resolve(name);
                 return $http.get(Constants.apiRootUrl)
                     .then(function(response) {
                             api.root = response.data;
-                            return api.resolve(name, item);
+                            return api.resolve(name);
                     })
             }
 
