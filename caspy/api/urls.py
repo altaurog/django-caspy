035b5e61bdda47652c104d11dc3ebc9b458f2720
Aryeh Leib Taurog <git@aryehleib.com>
2015-03-19 19:11:24 +0200

    Use hand-spun DRY url root

--- caspy/api/urls.py
+++ caspy/api/urls.py
@@ -1,14 +1,40 @@
+import re
 from django.conf.urls import patterns, url
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from . import views
+
+
+urlre_p1 = re.compile(r'\(\?P<\w+>.*\(\?#(:\w+)\)\)')
+urlre_p2 = re.compile(r'^\^|\$$')
+
+
+def rev(viewname):
+    for urlp in urlpatterns:
+        if urlp.name == viewname:
+            return urlre_p2.sub('', urlre_p1.sub(r'\g<1>', urlp._regex))
+    raise RuntimeError("No reverse match for %s" % viewname)
+
+
+def response(path, endpoints):
+    return Response({name: path + p for name, p in endpoints.items()})
+
+
+@api_view(('GET',))
+def api_root(request):
+    return response(request.path, {
+        'currency': rev('api-currency-detail'),
+    })
+
 
 urlpatterns = patterns('',  # noqa
     url(r'^$',
-        views.api_root,
+        api_root,
         name='api-root'),
     url(r'^currency/$',
         views.CurrencyList.as_view(),
         name='api-currency-list'),
-    url(r'^currency/(?P<pk>[A-Z]+|:code)/$',
+    url(r'^currency/(?P<pk>[A-Z]+(?#:code))/$',
         views.CurrencyDetail.as_view(),
         name='api-currency-detail'),
 )
--- caspy/api/views.py
+++ caspy/api/views.py
@@ -1,24 +1,6 @@
-try:
-    from urllib.parse import unquote  # python3
-except ImportError:
-    from urllib import unquote  # python2
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import generics
 from .. import models
 from . import serializers
-
-
-@api_view(('GET',))
-def api_root(request, format=None):
-    def rev(viewname, **kwargs):
-        url = reverse(viewname, kwargs=kwargs, request=request, format=format)
-        return unquote(url)
-
-    return Response({
-        'currency': rev('api-currency-detail', pk=':code'),
-    })
 
 
 class CurrencyList(generics.ListCreateAPIView):
