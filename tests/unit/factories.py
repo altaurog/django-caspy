84cec255a49031713443586d30336cdb923de07d
Aryeh Leib Taurog <git@aryehleib.com>
2015-04-15 22:43:14 +0300

    Use FactoryBoy for db tests

---
+++ tests/unit/factories.py
@@ -0,0 +1,47 @@
+import factory
+from caspy import models
+
+
+def letterstr(size, extra=''):
+    return lambda n: size * chr(65 + n) + extra
+
+
+class CurrencyFactory(factory.DjangoModelFactory):
+    cur_code = factory.Sequence(letterstr(3))
+    shortcut = factory.Sequence(letterstr(1))
+    symbol = factory.Sequence(letterstr(1))
+    long_name = factory.Sequence(letterstr(1, ' Currency'))
+
+    class Meta:
+        model = models.Currency
+
+
+class BookFactory(factory.DjangoModelFactory):
+    name = factory.Sequence(lambda n: 'Test Book %d' % n)
+
+    class Meta:
+        model = models.Book
+
+
+class AccountTypeFactory(factory.DjangoModelFactory):
+    account_type = factory.Iterator(['Credit Card', 'Bank Account', 'Income', 'Expense'])
+    sign = factory.Iterator([True, False])
+    credit_term = factory.Iterator(['charge', 'withdraw', 'income', 'credit'])
+    debit_term = factory.Iterator(['payment', 'deposit', 'income', 'debit'])
+
+    class Meta:
+        model = models.AccountType
+
+
+class AccountFactory(factory.DjangoModelFactory):
+    name = factory.Iterator(['Citibank Visa', 'Chase', 'Walmart', 'Rent'])
+    book = factory.SubFactory(BookFactory)
+    account_type = factory.SubFactory(AccountTypeFactory)
+    currency = factory.SubFactory(CurrencyFactory)
+
+    @factory.lazy_attribute
+    def description(self):
+        return self.name + ' account'
+
+    class Meta:
+        model = models.Account
--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -1,107 +1,72 @@
 import pytest
 from django.db.utils import IntegrityError
+from django.forms.models import model_to_dict
 from caspy import models
+import factories
 
 pytestmark = pytest.mark.django_db()
 
 
-currency = {
-        'cur_code': 'MM',
-        'shortcut': 'M',
-        'symbol': 'M',
-        'long_name': 'Monopoly Money',
-    }
-
-
 class TestCurrency:
-    mgr = models.Currency.objects
     def test_create_currency(self):
-        self.mgr.create(**currency)
-        assert self.mgr.filter(**currency).exists()
+        currency = factories.CurrencyFactory()
+        data = model_to_dict(currency)
+        assert models.Currency.objects.filter(**data).exists()
 
     unique_fields = ('cur_code', 'shortcut', 'long_name')
 
     @pytest.mark.parametrize('dupfield', unique_fields)
     def test_uniquness(self, dupfield):
-        self.mgr.create(**currency)
-        data = currency.copy()
-        data.update({f: 'A' for f in data.keys() if f != dupfield})
+        data = {dupfield: 'A'}
+        factories.CurrencyFactory(**data)
         with pytest.raises(IntegrityError):
-            self.mgr.create(**data)
+            factories.CurrencyFactory(**data)
 
 
 class TestBook:
     def test_create_book(self):
-        name = 'Test Book'
-        book_obj = models.Book.objects.create(name=name)
-        book_id = book_obj.book_id
-        now = book_obj.created_at
-        data = {'book_id': book_id, 'name': name, 'created_at': now}
+        book_obj = factories.BookFactory()
+        data = model_to_dict(book_obj)
         assert models.Book.objects.filter(**data).exists()
 
     def test_uniqueness(self):
         name = 'Test Book'
-        models.Book.objects.create(name=name)
+        factories.BookFactory(name=name)
         with pytest.raises(IntegrityError):
-            models.Book.objects.create(name=name)
-
-
-accounttype = {
-        'account_type': 'Income',
-        'sign': True,
-        'credit_term': 'income',
-        'debit_term': 'expense',
-    }
+            factories.BookFactory(name=name)
 
 
 class TestAccountType:
-    mgr = models.AccountType.objects
-
     def test_create_accounttype(self):
-        self.mgr.create(**accounttype)
-        assert self.mgr.filter(**accounttype).exists()
+        accounttype = factories.AccountTypeFactory()
+        data = model_to_dict(accounttype)
+        assert models.AccountType.objects.filter(**data).exists()
 
     def test_uniqueness(self):
-        self.mgr.create(**accounttype)
+        factories.AccountTypeFactory(account_type='Accounts Receivable')
         with pytest.raises(IntegrityError):
-            self.mgr.create(
-                    account_type='Income',
-                    sign=False,
-                    credit_term='credit',
-                    debit_term='debit',
-                )
+            factories.AccountTypeFactory(account_type='Accounts Receivable')
 
 
 class TestAccount:
-    mgr = models.Account.objects
-
-    def setup(self):
-        self.book = models.Book.objects.create(name='Test Book')
-        self.currency = models.Currency.objects.create(**currency)
-        self.accounttype = models.AccountType.objects.create(**accounttype)
-        self.data = {
-                'name': 'Salary',
-                'book': self.book,
-                'account_type': self.accounttype,
-                'currency': self.currency,
-                'description': 'Salary account',
-            }
-
     def test_create_account(self):
-        self.mgr.create(**self.data)
+        account_obj = factories.AccountFactory()
+        data = model_to_dict(account_obj)
+        assert models.Account.objects.filter(**data).exists()
 
     def test_create_with_empty_description(self):
-        data = self.data.copy()
-        del data['description']
-        self.mgr.create(**data)
+        factories.AccountFactory(description='')
 
-    def test_uniqueness(self):
-        self.mgr.create(**self.data)
-        data = self.data.copy()
-        data['name'] = 'Something different'
-        self.mgr.create(**data)
-        data = self.data.copy()
-        data['book'] = models.Book.objects.create(name='Different Book')
-        self.mgr.create(**data)
+    def test_unique_id(self):
+        account_obj = factories.AccountFactory()
         with pytest.raises(IntegrityError):
-            self.mgr.create(**self.data)
+            factories.AccountFactory(account_id=account_obj.account_id)
+
+    def test_unique_together(self):
+        account_obj = factories.AccountFactory()
+        name = account_obj.name
+        book = account_obj.book
+        factories.AccountFactory(name=name)
+        factories.AccountFactory(book=book)
+        with pytest.raises(IntegrityError):
+            factories.AccountFactory(name=name, book=book)
