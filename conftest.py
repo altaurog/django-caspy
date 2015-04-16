8b304b4e92f549d0d4673ad4c0cf50557778dae2
Aryeh Leib Taurog <git@aryehleib.com>
2015-04-16 12:40:00 +0300

    Create app for testing only

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
+import os
+import sys
 from django.conf import settings
 
 def pytest_configure():
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'tests'))
     settings.configure(
         INSTALLED_APPS = (
             'caspy',
             'rest_framework',
+            'testapp',
         ),
 
         ROOT_URLCONF = 'caspy.urls',
