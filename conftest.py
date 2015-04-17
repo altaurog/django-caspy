360901a7ac4f4c33e36817c9af7ad7cbd3029978
Aryeh Leib Taurog <git@aryehleib.com>
2015-04-17 16:18:35 +0300

    Add postgresql test

--- conftest.py
+++ conftest.py
@@ -4,22 +4,32 @@ from django.conf import settings
 
 def pytest_configure():
     sys.path.append(os.path.join(os.path.dirname(__file__), 'tests'))
-    settings.configure(
-        INSTALLED_APPS = (
+    test_settings = {
+        'INSTALLED_APPS': (
             'caspy',
             'rest_framework',
             'testapp',
         ),
 
-        ROOT_URLCONF = 'caspy.urls',
+        'ROOT_URLCONF': 'caspy.urls',
 
-        DATABASES = {
+        'DATABASES': {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
                 'NAME': ':memory:',
             }
         },
 
-        USE_TZ = True,
-        MIDDLEWARE_CLASSES = [],
-    )
+        'USE_TZ': True,
+        'MIDDLEWARE_CLASSES': [],
+    }
+
+    if os.getenv('USE_POSTGRESQL_DATABASE'):
+        test_settings['DATABASES']['default'] = {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': os.getenv('PGDATABASE'),
+                'USER': os.getenv('PGUSER'),
+                'HOST': os.getenv('PGHOST'),
+                'PASSWORD': os.getenv('PGPASSWORD'),
+            }
+    settings.configure(**test_settings)
