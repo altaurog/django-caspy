d7ab04186f3b8c7c58b654a7372b1d4f3ffad64e
Aryeh Leib Taurog <git@aryehleib.com>
2015-06-23 19:04:36 +0300

    Add unit tests for prepare_book

---
+++ tests/unit/test_domain_commands.py
@@ -0,0 +1,14 @@
+from caspy.domain import command, models
+
+class TestBook:
+    def test_prepare_new_book(self):
+        empty_book = models.Book()
+        result = command.prepare_book(empty_book, 'now')
+        assert isinstance(result, models.Book)
+        assert result.created_at == 'now'
+
+    def test_prepare_old_book(self):
+        dated_book = models.Book(created_at='last week')
+        result = command.prepare_book(dated_book, 'now')
+        assert isinstance(result, models.Book)
+        assert result.created_at == 'last week'
